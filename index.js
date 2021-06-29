dfb31885c255e9d10da74e73eeb42fab3994a938
Matthew Vaden <matthewvaden@gmail.com>
2021-06-28 20:31:42 -0400

    Create units of time.

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const countDown = () => {
    const currentDate = new Date().getTime();
    const timeDelta = futureDate - currentDate;
 
+   const second = 1000;
+   const minute = second * 60;
+   const hour   = minute * 60;
+   const day    = hour * 24;
+
+   const returnedDay = Math.floor(timeDelta / day);
 };
 
 countDown();
