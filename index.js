8ca00bf434f285d248698084af971cf7a1ccf5ae
Matthew Vaden <matthewvaden@gmail.com>
2021-06-28 20:30:55 -0400

    Get the delta for future date and current date.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const countDown = () => {
    const futureDate = new Date("April 24, 2022 00:00:00").getTime();
    const currentDate = new Date().getTime();
+   const timeDelta = futureDate - currentDate;
+
 };
 
 countDown();
